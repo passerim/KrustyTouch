0908e44bc2370814a9d0e7d7843457d1d2e65218
Andrea Stefanini <@studio.unibo.it>
2019-08-09 09:09:19 +0200

    added song that might be used as soundtrack and updated all files

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,9 +4,14 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Background;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,19 +23,26 @@ public class Main extends Application {
 		try {
 			Pane root= FXMLLoader.load(getClass().getResource("Scena.fxml"));
 
+
 			Scene scene = new Scene(root);
 			SpongebobManager thread = new SpongebobManager(root);
 			thread.start();
 			SpawnerPlanktonManager SpawnerManager = new SpawnerPlanktonManager(root);
 			SpawnerManager.start();
+
+
 			PrimaryStage.setHeight(1000);
 			PrimaryStage.setWidth(500);
 			PrimaryStage.setResizable(false);
 			PrimaryStage.setScene(scene);
 			PrimaryStage.sizeToScene();
 			PrimaryStage.show();
+			MediaPlayer musica = new MediaPlayer(new Media(new File("src/Images/SpongeBob_Soundtrack.mp3").toURI().toString()));
 
-			
+			musica.setVolume(100);
+
+			musica.play();
+
 
 			
 		} catch(Exception e) {
